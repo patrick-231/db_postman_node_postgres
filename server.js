2213ab7fcc2ca525e1f9aba8119dfc633a7f82a0
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-04-24 13:15:58 +0200

    I was able to get single customer with params

--- server.js
+++ server.js
@@ -10,18 +10,30 @@ app.use(express.json());
 
 const pool = new Pool();
 
-app.get('/customers', (req, res) =>{
-
-    pool.query('SELECT * FROM customers')
-    .then((data) => {
-        res.json(data.rows);
-    })
-    .catch((err) => {
-
-    console.error('Error:', err);
-    res.status(500).json({ error: 'An error occurred' });
+app.get('/customers', async (req, res) => {
+    try {
+      const data = await pool.query('SELECT * FROM customers');
+      res.json(data.rows);
+    } catch (err) {
+      console.error('Error:', err);
+      res.status(500).json({ error: 'An error occurred' });
+    }
   });
-});
+
+  app.get('/customers/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+    const data = await pool.query(' SELECT * FROM customers WHERE id=$1;', [id]);
+    res.json(data.rows[0]);
+        
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: 'An error occurred' });
+      }
+    
+  })
+
+  app.put('/customers')
 
 const PORT = process.env.PORT || 5000;
 
